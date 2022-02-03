02ce7220792dc7ef96182c0036b0ede02e16c283
m.habibi <m.habibi@mahsan.co>
2022-02-03 10:26:43 +0330

    Convert to persian

--- owghat.sh
+++ owghat.sh
@@ -34,5 +34,5 @@ curl -s 'https://www.bahesab.ir/mdn/time/azan/' \
   -H 'Referer: https://www.bahesab.ir/time/prayer/' \
   -H 'Accept-Language: en-US,en;q=0.9' \
   --data-raw 'string_o=%7B%22v%22%3A%7B%22city%22%3A%22%D8%AA%D9%87%D8%B1%D8%A7%D9%86%20%20%20%D8%A7%D8%B3%D8%AA%D8%A7%D9%86%20%D8%AA%D9%87%D8%B1%D8%A7%D9%86%22%2C%22day%22%3A0%2C%22Flag%22%3A%222022%2F1%2F31%22%7D%7D' \
-  --compressed | jq | head -11 | tail -8 | cut -d '"' -f2-4 | tr '"' ' ' | tr -s ' ' | sed -e 's/\:/=>/' 
+  --compressed | jq | head -11 | tail -8 | cut -d '"' -f2-4 | tr '"' ' ' | tr -s ' ' | sed -e 's/\:/=>/; s/Fajr/اذان صبح/; s/Sunrise/طلوع آفتاب/; s/Dhuhr/اذان ظهر/; s/Asr/اذان عصر/; s/Sunset/غروب آفتاب/; s/Maghrib/اذان معرب/; s/Isha/اذان عشاء/; s/Midnight/نیمه شب شرعی/; s/0/۰/g; s/1/۱/g; s/2/۲/g; s/3/۳/g; s/4/۴/g; s/5/۵/g; s/6/۶/g; s/7/۷/g; s/8/۸/g; s/8/۹/g;'
 echo '--------------------------------------------------------------------------'
